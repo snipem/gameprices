71c68012245c8cb1d1a9b1a254165bd932cdf089
shunkp <pshunk@gmail.com>
2014-05-01 23:06:51 -0400

    Argument support
    Add support for command line arguments (optional)

--- psnpricealert.py
+++ psnpricealert.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import logging
+import argparse
 
 version = sys.version_info[0]
 # import only once
@@ -18,10 +19,14 @@ logging.basicConfig(
     format = "%(asctime)s [%(levelname)-8s] %(message)s",
     filemode = "w")
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--cid", help="CID of game to check")
+parser.add_argument("--store", help="regional PSN store to check")
+parser.add_argument("--price", help="desired price of game", type=float)
+
 apiRoot = "https://store.sonyentertainmentnetwork.com/store/api/chihiro/00_09_000"
 
 def getJsonResponse(url):
-    # Python 2.x or 3.x support
     if version == 3:
         response = urlopen(url)
         enc = response.read().decode('utf-8')
@@ -46,12 +51,12 @@ def checkWishPrice(cid, store, wishPrice):
 
     if (currentPrice > wishPrice):
         if version == 3:
-            print("Wish price {0:.2f} does not yet match {1:.2f}, exiting".format(wishPrice, currentPrice))
+            print("Wish price {0:.2f} does not yet match current price {1:.2f}, exiting".format(wishPrice, currentPrice))
         else:
-    	    print("Wish price %.2f does not yet match %.2f, exiting" %(wishPrice, currentPrice))
+    	    print("Wish price %.2f does not yet match current price %.2f, exiting" %(wishPrice, currentPrice))
     else:
         if version == 3:
-            print("Wish price %{0:.2f} matched. Is now: %{1:.2f}".format(wishPrice, currentPrice))
+            print("Wish price {0:.2f} matched. Is now: {1:.2f}".format(wishPrice, currentPrice))
         else:
             print("Wish price %.2f matched. Is now: %.2f" %(wishPrice, currentPrice))
     	#TODO send alert
@@ -68,11 +73,10 @@ def getCidForName(name, store):
     links = data['categories']['games']['links']
 
     if (len(links) > 1):
-        # TODO: Let them select from list of results for proper CID?
         logging.error("Found more than one entry for name " + name)
         for link in links:
             logging.debug ("Found: " + link['default_sku']['entitlements'][0]['name'] + " - " + link['default_sku']['entitlements'][0]['id'])
-        raise Exception("More than one entry found for name '" + name + "'")
+        raise Exception("More than one entry found for name '" + name + "'. Check log file.")
     else:
         cid = links[0]['default_sku']['entitlements'][0]['id']
         logging.info("Found cid '"+cid+"' for name '"+name+"'")
@@ -81,8 +85,9 @@ def getCidForName(name, store):
 def prettyPrintJson(jsonString):
     print(json.dumps(jsonString, sort_keys=True,indent=4, separators=(',', ': ')))
 
-#Example
-store = "DE/de"
-cid = getCidForName("metal gear solid peace walker psp", store)
-wishPrice = 15.00
+args = parser.parse_args()
+# Read arguments or use example
+store = args.store if args.store else "DE/de"
+cid = args.cid if args.cid else getCidForName("metal gear solid peace walker psp", store)
+wishPrice = args.price if args.price else 15.00
 checkWishPrice(cid, store, wishPrice)
