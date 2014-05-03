dfcdc54bfe5a355ba58fc7fb4c9485c249564758
snipem <mail@matthias-kuech.de>
2014-05-03 11:27:09 +0200

    Replaced more UTF-8 sensitive print function

--- psnpricealert.py
+++ psnpricealert.py
@@ -14,8 +14,6 @@ if version == 3:
 elif version == 2:
     from urllib2 import urlopen
     from urllib2 import quote
-    #Set UTF-8 as standard output encoding, try to act as Python 3.x does
-    sys.stdout = codecs.getwriter("UTF-8")(sys.stdout)
 else:
     version == False
 
@@ -46,14 +44,14 @@ def checkWishPrice(cid, store, wishPrice):
     url = apiRoot + "/container/"+store+"/19/"+cid+"?size=99999"
     data = getJsonResponse(url)
 
-    print(data['default_sku']['entitlements'][0]['name'] + " - " + data['default_sku']['display_price'])
+    print_enc(data['default_sku']['entitlements'][0]['name'] + " - " + data['default_sku']['display_price'])
     currentPrice = float(data['default_sku']['price']) / 100
 
     if (currentPrice > wishPrice):
-        print("Wish price {0:.2f} does not yet match {1:.2f}, exiting".format(wishPrice, currentPrice))
+        print_enc("Wish price {0:.2f} does not yet match {1:.2f}, exiting".format(wishPrice, currentPrice))
         return False
     else:
-        print("Wish price %{0:.2f} matched. Is now: %{1:.2f}".format(wishPrice, currentPrice))
+        print_enc("Wish price %{0:.2f} matched. Is now: %{1:.2f}".format(wishPrice, currentPrice))
         return True
 
 def getCidForName(name, store):
@@ -75,7 +73,14 @@ def getCidForName(name, store):
         return cid
 
 def prettyPrintJson(jsonString):
-    print(json.dumps(jsonString, sort_keys=True,indent=4, separators=(',', ': ')))
+    print_enc(json.dumps(jsonString, sort_keys=True,indent=4, separators=(',', ': ')))
+
+def print_enc(uncoded):
+    if version == 2:
+        print (uncoded.encode("utf-8"))
+    else:
+        uncoded = uncoded +"\n"
+        sys.stdout.buffer.write(uncoded.encode('utf8'))
 
 def main():
     args = parser.parse_args()
@@ -90,7 +95,6 @@ def main():
             exit(0)
         else:
             exit(-1)
-
 
 if __name__ == "__main__":
     main()
