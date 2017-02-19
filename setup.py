743073e46c08eafc802a278fc8239940acb3ef9b
Matthias Küch <mail@matthias-kuech.de>
2017-02-19 10:57:42 +0100

    Ramp up for project rename

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup
+try:
+        import pypandoc
+        long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+        long_description = open('README.md').read()
+
 setup(
         name = 'psnprices',
         packages = ['psnprices'],
-        version = '1.0',
+        version = '0.2',
         description = 'An interface for the undocumented Sony PlayStation Store PSN Api',
+        long_description=long_description,
         author = 'Matthias Kuech',
         author_email = 'halde@matthias-kuech.de',
-        url = 'https://github.com/snipem/playstation-price-drop-alert',
-        download_url = 'https://github.com/snipem/playstation-price-drop-alert/tarball/0.1',
+        url = 'https://github.com/snipem/psnprices',
+        download_url = 'https://github.com/snipem/psnprices/archive/0.2.tar.gz',
         keywords = ['playstation', 'store', 'prices'],
         license = 'GPL2',
-        classifiers = [],
         test_suite='nose.collector',
         tests_require=['nose'],
         entry_points = {
