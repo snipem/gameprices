4adc176417bbb2292f33df11d30e6abfa931182c
Matthias Küch <mail@matthias-kuech.de>
2018-04-10 19:30:21 +0200

    Test for sending mail

--- psnprices/test/test_psn.py
+++ psnprices/test/test_psn.py
@@ -7,6 +7,13 @@ from psnprices.shops import psn
 from psnprices.shops.psn import Psn
 from psnprices.cli.psncli import psn_main
 
+from psnprices.cli.psnmailalert import main as psnmailalert_main
+
+from _pytest.monkeypatch import MonkeyPatch
+import pytest
+
+monkeypatch = MonkeyPatch()
+
 
 class MyTest(unittest.TestCase):
 
@@ -144,3 +151,50 @@ class PsnTest(unittest.TestCase):
             "'Bloodborne'"
             ]
         psn_main()
+
+    def test_mailalert(self):
+
+        import smtplib
+
+        filename = "alerts.csv"
+
+        f = open(filename,"w")
+        f.write("EP0177-CUSA07010_00-SONICMANIA000000,100.00")
+        f.close()
+
+        f = open("mailconfig.json","w")
+        f.write("""
+        {
+            "from":"from@example.com",
+            "to":"to@example.com",
+            "username":"from_user",
+            "password":"from_password",
+            "server":"smtp.gmail.com"
+        }
+        """)
+        f.close()
+
+        def mocklogin(cl, user, password):
+            return True
+
+        def mockreturn(cl, frm, to, msg):
+            assert "from@example.com" == frm
+            assert "to@example.com" == to
+            assert "Price Drop" in msg
+
+        monkeypatch.setattr(smtplib.SMTP, 'login', mocklogin)
+        monkeypatch.setattr(smtplib.SMTP, 'sendmail', mockreturn)
+
+        sys.argv = [
+            "psnmailalert",
+            ]
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            psnmailalert_main()
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 0 
+
+        data = "" 
+        with open(filename, 'r') as myfile:
+            data = data + myfile.read()
+
+        assert "" == data
