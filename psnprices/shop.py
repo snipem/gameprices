c53fdebf8c3b1c6ef07b7984d75f4d74338cd9d8
Matthias Küch <mail@matthias-kuech.de>
2018-04-14 12:08:28 +0200

    Removed unused inherited search method

--- psnprices/shop.py
+++ psnprices/shop.py
@@ -7,9 +7,3 @@ class Shop(object):
     def __init__(self, country):
         super(Shop, self).__init__()
         self.country = country
-
-    def get_item_by(self, id, name):
-        game_offers = self.search(name)
-        for game_offer in game_offers:
-            if game_offer.id == id:
-                return game_offer
